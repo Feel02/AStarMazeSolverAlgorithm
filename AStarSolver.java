ccb14d051d358c23a947ef55c87c9a5b14dec5f9
Enes Sapan <115135140+Feel02@users.noreply.github.com>
2023-12-09 22:55:25 +0300

    Add files via upload

---
+++ AStarSolver.java
@@ -0,0 +1,243 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 4hm3t
+ */
+import java.util.AbstractCollection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+public class AStarSolver extends Solver
+{
+	/*
+	 * Constructor
+	 * m: The maze to solve
+	 */
+	public AStarSolver(Maze m, Boolean manhattan)
+	{
+		this.maze = m;
+		this.result = "";
+		this.manhattan = manhattan; //manhattan distance or euclidean distance for the heuristic func
+		this.frontier = new PriorityQueue<Node<Maze>>(new Comparator<Node<Maze>>()
+		{
+			public int compare(Node<Maze> s1, Node<Maze> s2) 
+		    	{
+		    		Double sf1 = s1.getContent().getCurrState().getF();
+		    		Double sf2 = s2.getContent().getCurrState().getF();
+		    		Double sh1 = s1.getContent().getCurrState().getH();
+		    		Double sh2 = s2.getContent().getCurrState().getH();
+		    	
+		    		if(sf1 > sf2)
+		    			return 1;
+		    		else if(sf1 == sf2)
+		    		{
+		    			if(sh1 > sh2)
+		    				return 1;
+		    			else if(sh1 == sh2)
+		    				return 0;
+		    			else
+		    				return -1;
+		    		}
+		    		else
+		    			return -1;
+		    	}
+		});
+		this.closedSquares = new PriorityQueue<Square>(new Comparator<Square>()
+		{
+			public int compare(Square s1, Square s2) 
+		    	{
+				Double sf1 = s1.getF();
+		    		Double sf2 = s2.getF();
+		    		Double sh1 = s1.getH();
+		    		Double sh2 = s2.getH();
+		    	
+		    		if(sf1 > sf2)
+		    			return 1;
+		    		else if(sf1 == sf2)
+		    		{
+		    			if(sh1 > sh2)
+		    				return 1;
+		    			else if(sh1 == sh2)
+		    				return 0;
+		    			else
+		    				return -1;
+		    		}
+		    		else
+		    			return -1;
+		    	}
+		});
+	}
+	
+	public String solve()
+	{
+		this.maze.initMaze(); //Init maze
+		
+		Boolean endfound = false;
+		this.nodesCounter = 0;
+		this.pathLength = 0;
+		
+		//Compute h (so f due to g=0) and f value of Starting square
+		if(manhattan) //Check whether manhattan distance or euclidean distance for the heuristic func
+			this.maze.getStart().calcManhattanH();
+		else
+			this.maze.getStart().calcEuclidH();
+		
+		this.maze.getStart().calcF();//calculate f for the starting square
+		
+		//Init data structures
+		this.frontier.clear(); //Clear frontier Queue - frontier is fringe (priority queue)
+		((PriorityQueue<Node<Maze>>) this.frontier).offer(new Node<Maze>(this.maze)); //Adding the first node (Start node) (g is at 0, [Start to Start = 0])
+		this.closedSquares.clear(); //Clear closedSquares - stack for the visited ones aka explored set
+		
+		//Measure run time
+		long startTime = System.currentTimeMillis();
+		
+		while(!endfound)
+		{
+                        //you should check if there exist a node to expand
+			if(this.frontier.isEmpty())
+				break;
+			else
+			{
+                                //You should first remove it from the queue (you visit it)
+				Node<Maze> current = ((PriorityQueue<Node<Maze>>) this.frontier).remove(); //Get first node from the frontier
+				this.maze = (Maze) current.getContent(); //Get maze from the node
+				Square currState = this.maze.getCurrState(); //Get current state from the maze
+				
+                                //checking if we have found the solution
+				if(currState.getCol() == this.maze.getEnd().getCol() && currState.getLine() == this.maze.getEnd().getLine())
+				{
+					//You should create a new Node<T> for this maze to be able to assign a new father to it
+					//Set current as father (parent) for all next states
+					//add the goal (end) so you can reach starting point by using fathers (parents)
+					endfound = true;
+				}
+				else
+				{
+					LinkedList<Node<Maze>> nexts = this.getNextSquares(); //Get next possible states
+                                        
+                                        //now the current one was visited (that you want to explore its next states)
+                                        //So it should be added to other queue (which keeps track the visited ones)
+                                        //do not forget to check if it is already in
+                                        //do not forget to set * for the visited one (current one) (is it gonna be the Node or Square?)
+                                                
+                                        //You may want to use an iterator to reach every possible next state one by one 
+                                        //or you may want to get size of the next possible states and reach them accordingly
+                                                
+                                        //Add next possible states to the queue 
+                                        //(think about which queue it could be.)
+                                        //Do not forget to add all next possible states (at most they are: n e s w)
+                                        //-> to be able to have n e s w, you should iterate it from start to end and add it like that
+                                        //Do not forget to set father (parent) node of the added ones before adding (as current node)
+                                        //Do not forget to increment nodesCounter
+
+                                        //You may need typecasting for the PriorityQueue (before adding) as can be observed from given codes
+				}
+			}
+		}
+		long endTime = System.currentTimeMillis();
+		
+		long time = endTime - startTime;
+		
+		if(this.manhattan)
+			this.result = "    ___                    __  ___            __          __  __            \r\n" + 
+					"   /   | __/|_            /  |/  /___ _____  / /_  ____ _/ /_/ /_____ _____ \r\n" + 
+					"  / /| ||    /  ______   / /|_/ / __ `/ __ \\/ __ \\/ __ `/ __/ __/ __ `/ __ \\\r\n" + 
+					" / ___ /_ __|  /_____/  / /  / / /_/ / / / / / / / /_/ / /_/ /_/ /_/ / / / /\r\n" + 
+					"/_/  |_||/             /_/  /_/\\__,_/_/ /_/_/ /_/\\__,_/\\__/\\__/\\__,_/_/ /_/ \n";
+		else
+			this.result = "    ___                    ______           ___     __\r\n" + 
+					"   /   | __/|_            / ____/_  _______/ (_)___/ /\r\n" + 
+					"  / /| ||    /  ______   / __/ / / / / ___/ / / __  / \r\n" + 
+					" / ___ /_ __|  /_____/  / /___/ /_/ / /__/ / / /_/ /  \r\n" + 
+					"/_/  |_||/             /_____/\\__,_/\\___/_/_/\\__,_/   \n";
+		//You should add the result to the String variable "result" 
+                //which is going to be printed to a text file
+		if(endfound)
+		{
+			this.maze.resetGrid();
+			Node<Maze> revertedTree = ((PriorityQueue<Node<Maze>>) this.frontier).remove();
+			
+			revertedTree = revertedTree.getFather();
+			this.result += "Path: " + this.maze.getEnd().toString() + "(End) <- ";
+			this.pathLength++;
+			
+			while(revertedTree.hasFather())
+			{
+				Maze temp = revertedTree.getContent();
+				Square state = temp.getCurrState();
+				
+				if(!state.equals(this.maze.getEnd()))
+				{
+					this.result += state.toString() + " <- ";
+					this.maze.getGrid()[state.getLine()][state.getCol()].setAttribute("*");
+					this.pathLength++;
+				}
+				revertedTree = revertedTree.getFather();
+			}
+			
+			this.result += this.maze.getStart().toString() + "(Start) \n" + "Path length: " + this.pathLength + "\nNumber of nodes created: " + this.nodesCounter + "\nExecution time: " + time/1000d + " seconds\n";
+			this.result += this.maze.printMaze();
+		}
+		else
+		{
+			this.result += "Failed : Unable to go further and/or end is unreachable.";
+		}
+		
+		return this.result;
+	}
+	
+	/*
+	 *  Get the next ("walkables") squares from the given square
+	 *  c: Square from where to get the nexts squares
+	 */
+	public LinkedList<Node<Maze>> getNextSquares()
+	{
+		LinkedList<Node<Maze>> res = new LinkedList<Node<Maze>>();
+		
+		//Get 4 next squares
+		LinkedList<Maze> nexts = this.maze.getCurrState().getNexts();
+		
+		int gCurrent = this.maze.getCurrState().getG();
+		
+		for(int i = 0; i < nexts.size(); i++)
+		{
+			Square tempSq = nexts.get(i).getCurrState();
+			if(!this.closedSquares.contains(tempSq))
+			{
+				if(this.manhattan)
+					nexts.get(i).getCurrState().calcManhattanH();
+				else
+					nexts.get(i).getCurrState().calcEuclidH();
+				
+				nexts.get(i).getCurrState().incG(gCurrent);
+				
+				nexts.get(i).getCurrState().calcF();
+				
+				Node<Maze> tempNode = new Node<Maze>(nexts.get(i));
+				res.add(tempNode);
+			}
+		}
+		
+		return res;
+	}
+
+	public String getResult() 
+	{
+		if(this.result == "")
+			return "No resolution computed, use the solve method first";
+		else
+			return this.result;
+	}
+	
+	public AbstractCollection<Node<Maze>> getFrontier() 
+	{
+		return this.frontier;
+	}
+}
---
+++ Main.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 4hm3t
+ */
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main 
+{
+	public static void main(String[] args) throws IOException 
+	{   
+            Maze mazeFromText = new Maze("./maze.txt");
+            Maze mazeFromText2 = new Maze("./maze2.txt");
+            
+            //BFS_Solver bfs = new BFS_Solver(mazeFromText);
+            //DFS_Solver dfs = new DFS_Solver(mazeFromText);
+            AStarSolver astar = new AStarSolver(mazeFromText, true);
+            AStarSolver astar_euclid = new AStarSolver(mazeFromText, false);
+            
+            //BFS_Solver bfs2 = new BFS_Solver(mazeFromText2);
+            //DFS_Solver dfs2 = new DFS_Solver(mazeFromText2);
+            AStarSolver astar2 = new AStarSolver(mazeFromText2, true);
+            AStarSolver astar2_euclid = new AStarSolver(mazeFromText2, false);
+                
+            //String bfs_res = bfs.solve();
+            //String dfs_res = dfs.solve();
+            String astar_res = astar.solve();
+            String astar_res_euclid = astar_euclid.solve();
+            
+            //String bfs_res2 = bfs2.solve();
+            //String dfs_res2 = dfs2.solve();
+            String astar_res2 = astar2.solve();
+            String astar_res2_euclid = astar2_euclid.solve();
+                
+            //Path bfs_path = Paths.get("./bfs.txt");
+            //Path dfs_path = Paths.get("./dfs.txt");
+            Path astar_path = Paths.get("./astar.txt");
+            Path astar_path_euclid = Paths.get("./astar_euclid.txt");
+            
+            //Path bfs_path2 = Paths.get("./bfs2.txt");
+            //Path dfs_path2 = Paths.get("./dfs2.txt");
+            Path astar_path2 = Paths.get("./astar2.txt");
+            Path astar_path2_euclid = Paths.get("./astar2_euclid.txt");
+            
+            try 
+            {
+                // Now calling Files.writeString() method
+                // with path , content & standard charsets
+                //Files.writeString(bfs_path, bfs_res, StandardCharsets.UTF_8);
+                //Files.writeString(dfs_path, dfs_res, StandardCharsets.UTF_8);
+                Files.writeString(astar_path, astar_res, StandardCharsets.UTF_8);
+                Files.writeString(astar_path_euclid, astar_res_euclid, StandardCharsets.UTF_8);
+                
+                //Files.writeString(bfs_path2, bfs_res2, StandardCharsets.UTF_8);
+                //Files.writeString(dfs_path2, dfs_res2, StandardCharsets.UTF_8);
+                Files.writeString(astar_path2, astar_res2, StandardCharsets.UTF_8);
+                Files.writeString(astar_path2_euclid, astar_res2_euclid, StandardCharsets.UTF_8);
+            }            
+            // Catch block to handle the exception
+            catch (IOException ex) 
+            {
+                // Print messqage exception occurred as
+                // invalid. directory local path is passed
+                System.out.print("Invalid Path");
+            }
+            
+            System.out.println("Completed!");
+	}
+}
+
